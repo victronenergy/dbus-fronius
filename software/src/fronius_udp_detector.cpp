45317388cfdad3df74cd66002946e00d9ef9c3d5
Izak Burger <isburger@gmail.com>
2019-11-25 14:42:53 +0200

    Add QT4 backwards compatibility for UDP scanning

--- software/src/fronius_udp_detector.cpp
+++ software/src/fronius_udp_detector.cpp
@@ -3,7 +3,9 @@
 #include <QTimer>
 #include <QByteArray>
 #include <QUdpSocket>
-#include <QNetworkDatagram>
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+	#include <QNetworkDatagram>
+#endif
 #include <QsLog.h>
 #include "fronius_udp_detector.h"
 
@@ -16,7 +18,7 @@ FroniusUdpDetector::FroniusUdpDetector(QObject *parent) :
 	connect(mTimeout, SIGNAL(timeout()), this, SIGNAL(finished()));
 
 	// Fronius inverters respond on this port
-	mUdpSocket->bind(QHostAddress::AnyIPv4, 50050, QUdpSocket::ReuseAddressHint);
+	mUdpSocket->bind(QHostAddress::Any, 50050, QUdpSocket::ReuseAddressHint);
 	connect(mUdpSocket, SIGNAL(readyRead()), this, SLOT(responseReceived()));
 }
 
@@ -35,7 +37,15 @@ void FroniusUdpDetector::start()
 void FroniusUdpDetector::responseReceived()
 {
 	while (mUdpSocket->hasPendingDatagrams()) {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
 		QNetworkDatagram datagram = mUdpSocket->receiveDatagram();
 		mDevicesFound.insert(datagram.senderAddress());
+#else
+		QHostAddress addr;
+		// if length is zero packet is discarded. We care only about the
+		// address.
+		mUdpSocket->readDatagram(0, 0, &addr);
+		mDevicesFound.insert(addr);
+#endif
 	}
 }
