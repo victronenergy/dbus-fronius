7e42f1c5d0920832717745ad85450be7d212c73a
Izak Burger <iburger@victronenergy.com>
2023-10-30 23:01:27 +0200

    clean up unused constants

--- software/src/defines.h
+++ software/src/defines.h
@@ -65,8 +65,6 @@ struct DeviceInfo
 
 /// This value is used to indicate that the correct device instance has not
 /// been set yet.
-const int InvalidDeviceInstance = -1;
 const int MinDeviceInstance = 20;
-const int MaxDeviceInstance = 79;
 
 #endif // DEFINES_H
