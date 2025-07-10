c107a5046cebedc9769ea9a940ca9a53a20ed77f
c.warmuth <c.warmuth@applord-it.at>
2025-07-10 20:22:38 +0200

    Check if inverter is Solaredge

--- software/src/sunspec_detector.cpp
+++ software/src/sunspec_detector.cpp
@@ -104,7 +104,7 @@ void SunspecDetector::onFinished()
 			// to finish the detection. If we already have model 1 and a
 			// measurement model this will succeed.
 			if (di->di.productId != 0) {
-                if (di->di.powerLimitScale==0)
+                if (di->di.productId == VE_PROD_ID_PV_INVERTER_SOLAREDGE && di->di.powerLimitScale==0)
                     di->di.powerLimitScale=100.0f; // Set Scale to 100.0 if not set for Solaredge.
 				checkDone(di);
 				return;
