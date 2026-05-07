c492f70efa1eebc10be7b58f710b8b39011bbee2
Izak Burger <iburger@victronenergy.com>
2026-05-07 10:10:31 +0200

    remove old unused FroniusNullFrame
    This was moved to sunspec_updater.cpp long ago.

--- software/src/fronius_inverter.cpp
+++ software/src/fronius_inverter.cpp
@@ -1,12 +1,6 @@
 #include "ve_service.h"
 #include "fronius_inverter.h"
 
-// Fronius inverters send a null payload during certain solar net timeouts. We
-// want to filter for those.
-static const QVector<quint16> FroniusNullFrame = {
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7 };
-
 
 FroniusInverter::FroniusInverter(VeQItem *root, const DeviceInfo &deviceInfo,
 					int deviceInstance, QObject *parent) :
